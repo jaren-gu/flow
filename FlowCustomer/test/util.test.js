5d5f3f65349bac52d94d865ce131717884425260
jaren <jaren@localhost.localdomain>
2017-08-15 22:23:14 +0800

    util test

--- FlowCustomer/test/util.test.js
+++ FlowCustomer/test/util.test.js
@@ -3,14 +3,104 @@ let should = require('should');
 
 describe('test/util.js', () => {
     /**
-     * isNumber 测试
+     * isObject
+     */
+    it('isObject("2") should be false', () => {
+        util.isObject('2').should.equal(false);
+    })
+
+    it('isObject({}) should be true', () => {
+        util.isObject({}).should.equal(true);
+    })
+
+    /**
+     * isFunction
+     */
+    it('isFunction("2") should be false', () => {
+        util.isFunction('2').should.equal(false);
+    })
+
+    it('isFunction(()=>{}) should be true', () => {
+        util.isFunction(() => {}).should.equal(true);
+    })
+
+    /**
+     * isArray
+     */
+    it('isArray("2") should be false', () => {
+        util.isArray('2').should.equal(false);
+    })
+
+    it('isArray([]) should be true', () => {
+        util.isArray([]).should.equal(true);
+    })
+
+    /**
+     * isBool
+     */
+    it('isBool(2) should be false', () => {
+        util.isBool(2).should.equal(false);
+    })
+
+    it('isBool(false) should be true', () => {
+        util.isBool(false).should.equal(true);
+    })
+
+    /**
+     * isString
+     */
+    it('isString(2) should be false', () => {
+        util.isString(2).should.equal(false);
+    })
+
+    it('isString("2") should be true', () => {
+        util.isString('2').should.equal(true);
+    })
+
+    /**
+     * isNumber
      */
     it('isNumber(2) should be true', () => {
         util.isNumber(2).should.equal(true);
     })
 
     it('isNumber("2") should be false', () => {
-            util.isNumber('2').should.equal(false);
+        util.isNumber('2').should.equal(false);
+    })
+
+    /**
+     * isEmpty
+     */
+    it('isEmpty([1]) should be false', () => {
+        util.isEmpty([1]).should.equal(false);
     })
     
+    it('isEmpty({1:1}) should be false', () => {
+        util.isEmpty({1:1}).should.equal(false);
+    })
+
+    it('isEmpty("1") should be false', () => {
+        util.isEmpty('1').should.equal(false);
+    })
+
+    it('isEmpty("") should be true', () => {
+        util.isEmpty('').should.equal(true);
+    })
+
+    it('isEmpty({}) should be true', () => {
+        util.isEmpty({}).should.equal(true);
+    })
+
+    it('isEmpty(undefined) should be true', () => {
+        util.isEmpty(undefined).should.equal(true);
+    })
+
+    it('isEmpty(null) should be true', () => {
+        util.isEmpty(null).should.equal(true);
+    })
+
+    it('isEmpty([]) should be true', () => {
+        util.isEmpty([]).should.equal(true);
+    })
+
 })
--- FlowCustomer/utils/utils.js
+++ FlowCustomer/utils/utils.js
@@ -1,7 +1,79 @@
-function isNumber (arg){
-    return typeof arg === 'number'
+'use strict';
+
+var toString = Object.prototype.toString;
+
+/**
+ * 判断传入值是否对象
+ * @param {object} obj 
+ */
+function isObject(obj) {
+    return obj !== null && typeof obj === 'object';
+}
+
+/**
+ * 判断对象是否数组
+ * @param {object} obj 
+ */
+function isArray(obj) {
+    return toString.call(obj) === '[object Array]';
+}
+
+/**
+ * 判断对象是否函数
+ * @param {object} obj 
+ */
+function isFunction(obj){
+    return toString.call(obj) === '[object Function]';
+}
+
+/**
+ * 判断对象是否布尔值
+ * @param {object} obj 
+ */
+function isBool(obj){
+    return toString.call(obj) === '[object Boolean]';
+}
+
+/**
+ * 判断对象是否字符串
+ * @param {Object} obj 
+ */
+function isString(obj) {
+    return typeof obj === 'string';
+}
+
+/**
+ * 判断对象是否数值
+ * @param {object} obj 
+ */
+function isNumber(obj) {
+    return typeof obj === 'number';
+}
+
+/**
+ * 检查传入值是否为空
+ * @param {object|string} obj 
+ */
+function isEmpty(obj){
+    if (obj === null)
+        return true;
+
+    if (typeof obj === 'undefined')
+        return true;
+
+    if(typeof obj === 'object')
+        return Object.keys(obj).length === 0;
+    
+    if (typeof obj === 'string')
+        return obj.length === 0;
 }
 
 module.exports = {
-    isNumber : isNumber
+    isObject,
+    isFunction,
+    isArray,
+    isBool,
+    isString,
+    isNumber,
+    isEmpty
 }
